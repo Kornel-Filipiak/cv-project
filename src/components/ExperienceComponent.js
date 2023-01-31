8eccab7242224774215d66693552b1216ecbdfe1
Kornel Filipiak <kornel@kidoodle.tv>
2023-01-31 14:45:19 -0700

    update experience component methods

--- src/components/ExperienceComponent.js
+++ src/components/ExperienceComponent.js
@@ -1,95 +1,133 @@
 import React, { useState } from 'react';
 import { Print, NoPrint } from 'react-easy-print';
+import TextSection from './TextComponent';
 
 const ExperienceSection = (props) => {
+
+	const [experience, setExperience] = useState({
+		companyName: '',
+		position: '',
+		startDate: '',
+		endDate: '',
+	});
+
+	const [editMode, setEditMode] = useState(false);
+
     const handleChange = (e) => {
 		const { name, value } = e.target;
-        //update the the field name value
-    }
+		setExperience((prevExperience) => {
+			return {
+				...prevExperience,
+				[name]: value,
+			};
+		});
+	};
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //submit form
-    };
+	const handleEdit = (e) => {
+		e.preventDefault();
+		setEditMode(true);
+	};
 
-    return (
-		<NoPrint>
-			<Print printOnly>
-				<p className='warning'>Fill in the form.</p>
-			</Print>
-			<section>
-				<form 
-                    action='' 
-                    className='section xpSection' 
-                    // onSubmit={/* */}
-                    >
-					<label>
-						<p>Company Name:</p>
-						<input
-							type='text'
-							placeholder='Company Name'
-							name='companyName'
-							// onChange={/* */}
-							// value={/* */}
+	const { companyName, position, startDate, endDate } = experience;
+	const { text, handleDelete } = props;
+
+	if (editMode) {
+		return (
+			<NoPrint>
+				<Print printOnly>
+					<p className='warning'>Fill in the form.</p>
+				</Print>
+				<section>
+					<form 
+						action='' 
+						className='section xpSection' 
+						onSubmit={handleEdit}
+						>
+						<label>
+							<p>Company Name:</p>
+							<input
+								type='text'
+								placeholder='Company Name'
+								name='companyName'
+								onChange={handleChange}
+								value={companyName}
+								required
+							/>
+						</label>
+
+						<label>
+							<p>Position Title:</p>
+							<input
+								type='text'
+								placeholder='Position Title'
+								name='position'
+								onChange={handleChange}
+								value={position}
+								required
+							/>
+						</label>
+
+						<label>
+							<p>From:</p>
+							<input
+								type='date'
+								name='from'
+								placeholder='From'
+								onChange={handleChange}
+								value={startDate}
+								required
+							/>
+						</label>
+
+						<label>
+							<p>To:</p>
+							<input
+								type='date'
+								name='to'
+								placeholder='To'
+								onChange={handleChange}
+								value={endDate}
+								required
+							/>
+						</label>
+						<textarea
+							name='tasks'
+							className='tasks'
+							placeholder='Main Tasks'
+							onChange={handleChange}
+							value={text}
 							required
 						/>
-					</label>
-
-					<label>
-						<p>Position Title:</p>
-						<input
-							type='text'
-							placeholder='Position Title'
-							name='position'
-							// onChange={/* */}
-							// value={/* */}
-							required
-						/>
-					</label>
-
-					<label>
-						<p>From:</p>
-						<input
-							type='date'
-							name='from'
-							placeholder='From'
-							// onChange={/* */}
-							// value={/* */}
-							required
-						/>
-					</label>
-
-					<label>
-						<p>To:</p>
-						<input
-							type='date'
-							name='to'
-							placeholder='To'
-							// onChange={/* */}
-							// value={/* */}
-							required
-						/>
-					</label>
-					<textarea
-						name='tasks'
-						className='tasks'
-						placeholder='Main Tasks'
-							// onChange={/* */}
-							// value={/* */}
-						required
+						<button className='formBtn'>Save</button>
+						<button
+							className='formBtn'
+							type='button'
+							onClick={() => handleDelete(props.id)}
+							>
+							Delete
+						</button>
+					</form>
+				</section>
+			</NoPrint>
+		);
+	} else {
+		return (
+			<Print>
+				<section>
+					<TextSection
+						companyName={companyName}
+						position={position}
+						startDate={startDate}
+						endDate={endDate}
+						text={text}
+						handleEdit={handleEdit}
+						handleDelete={handleDelete}
+						id={props.id}
 					/>
-					<button className='formBtn'>Save</button>
-					<button
-						className='formBtn'
-						type='button'
-						// onClick={/* */}
-                        >
-						Delete
-					</button>
-				</form>
-			</section>
-		</NoPrint>
-	);
+				</section>
+			</Print>
+		);
+	}
 };
 
 export default ExperienceSection;
